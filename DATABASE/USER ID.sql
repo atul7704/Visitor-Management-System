a5661879c5f3812586e91de816517d5cd219cb59
atul7704 <166157306+atul7704@users.noreply.github.com>
2025-02-18 12:47:57 +0530

    Update USER ID.sql

--- DATABASE/USER ID.sql
+++ DATABASE/USER ID.sql
@@ -1,1 +1,7 @@
+create table UserLogin(uid serial,email varchar(30),password varchar(30))
+insert into UserLogin(email,password)values('atulsingh565@gmail.com','AKot@2002')
+insert into UserLogin(email,password)values('aK5657@gmail.com','singh@2002')
+insert into UserLogin(email,password)values('POOJA6@gmail.com','king@2002')
+select * from UserLogin
 
+drop table UserLogin
